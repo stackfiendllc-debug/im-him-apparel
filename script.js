7b5436a5057e9314f04294c16f5e863f5ee44822
stackfiendllc-debug <stackfiendllc@gmail.com>
2026-05-29 11:01:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-document.querySelector('.tv-logo').addEventListener('mouseover', () => {
-  document.querySelector('.tv-logo').style.transform = 'scale(1.05)';
+const logo = document.querySelector('.tv-logo');
+
+logo.addEventListener('mouseover', () => {
+  logo.style.transform = 'scale(1.08)';
 });
 
-document.querySelector('.tv-logo').addEventListener('mouseout', () => {
-  document.querySelector('.tv-logo').style.transform = 'scale(1)';
+logo.addEventListener('mouseout', () => {
+  logo.style.transform = 'scale(1)';
 });
+
+setInterval(() => {
+  logo.style.filter = Math.random() > 0.5
+    ? 'drop-shadow(0 0 30px orange)'
+    : 'drop-shadow(0 0 15px gold)';
+}, 500);
